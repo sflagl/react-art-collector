fbb465139bfdbc8f9ba4ba0e5fdbc127e81e76a3
sflagl <samanthaflagler@gmail.com>
2022-12-03 14:57:55 -0500

    initial repo creation

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+const ArtCollector = () => {
+  return(
+    <div>Hello Collector</div>
+  )
+}
+
+ReactDOM.render(
+  <ArtCollector />,
+  document.getElementById('artCollector')
+)
